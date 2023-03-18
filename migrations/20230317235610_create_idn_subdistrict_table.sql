2c621ca3e1eafbf93383dcf6bfa2d75f0c118e43
ruriazz <azizruri.s@gmail.com>
2023-03-18 16:51:47 +0700

    init idn subdistict and urban villages

--- migrations/20230317235610_create_idn_subdistrict_table.sql
+++ migrations/20230317235610_create_idn_subdistrict_table.sql
@@ -1,12 +1,11 @@
 -- +goose Up
 START TRANSACTION;
 
-    CREATE TABLE `idn_district` (
+    CREATE TABLE `idn_subdistrict` (
         `pkid` SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-        `idn_province_pkid` TINYINT UNSIGNED NOT NULL,
+        `idn_district_pkid` SMALLINT UNSIGNED NOT NULL,
         `slug` VARCHAR(75) NULL DEFAULT NULL,
-        `code` VARCHAR(5) NULL DEFAULT NULL,
-        `dati_name` VARCHAR(20) NOT NULL,
+        `code` VARCHAR(12) NULL DEFAULT NULL,
         `name` VARCHAR(75) NOT NULL,
         `alias` VARCHAR(75) NULL DEFAULT NULL,
         `created_at` DATETIME(6) NOT NULL DEFAULT NOW(6),
@@ -14,12 +13,12 @@ START TRANSACTION;
         `deleted_at` DATETIME(6) NULL DEFAULT NULL,
         PRIMARY KEY (`pkid`),
         UNIQUE (`slug`, `code`),
-        FOREIGN KEY (`idn_province_pkid`) REFERENCES `idn_province`(`pkid`)
+        FOREIGN KEY (`idn_district_pkid`) REFERENCES `idn_district`(`pkid`)
     );
 
 COMMIT;
 
 -- +goose Down
 START TRANSACTION;
-    DROP TABLE `idn_district`;
+    DROP TABLE `idn_subdistrict`;
 COMMIT;
